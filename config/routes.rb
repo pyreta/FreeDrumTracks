fc4c4c8192716eb9d51c4447034548094df6a1ff
Louis Abramson <pyreta@gmail.com>
2016-09-08 11:57:44 -0400

    Action Mailer Setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "static_pages#root"
 
-    namespace :api, defaults: {format: :json} do
-      resources :users, only:[:create, :show] do
-      end
-    end
+    # namespace :api, defaults: {format: :json} do
+    #   resources :users, only:[:create, :show] do
+    #   end
+    # end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
